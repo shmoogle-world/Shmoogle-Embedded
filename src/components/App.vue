29b461c67308d3e5a1367e10134aa0f4a1fbabb5
Fadi AT <fadiatamny@gmail.com>
2019-12-14 14:29:46 +0200

    Better Error message handling
    Co-authored-by: Elias Renman <elias.renman00@gmail.com>

--- src/components/App.vue
+++ src/components/App.vue
@@ -76,8 +76,8 @@ export default {
                 self.renderModal();
             }
             else {
-                console.log(this.response);
-                self.error = response.error;
+                console.log(this.response.error);
+                self.error = 'Something went wrong!';
                 self.errorOpen = true;
             }
             
